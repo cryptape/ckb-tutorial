ba2af8526a99806f66ee0fb5a701131d331d8d33
lee920217 <francislee920217@gmail.com>
2023-07-03 21:42:23 +0800

    update NextPage component match docs.json

--- src/components/NextPage/NextPage.tsx
+++ src/components/NextPage/NextPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import docs from '../../utils/docs.ts'
-import './NextPage.scss'
+import docs, { Doc } from '../../utils/docs.ts';
+import './NextPage.scss';
 
 interface NextPageProps {
     docName: string;
@@ -9,25 +9,29 @@ interface NextPageProps {
 
 const NextPage: React.FC<NextPageProps> = ({ docName }) => {
     const navigate = useNavigate();
-    const [route, setRoute] = useState({ firstLevel: '', secondLevel: '' });
+    const [route, setRoute] = useState<Doc | null>(null);
 
     useEffect(() => {
-        const docRoute = docs.find(doc => doc.name === docName);
+        const docRoute = docs.find(doc => doc.secondLevels.some(level => level.name === docName));
         if (docRoute) setRoute(docRoute);
     }, [docName]);
 
     const handleClick = () => {
-        navigate(route.secondLevel);
+        if (route && route.secondLevels.length > 0) {
+            navigate(route.secondLevels[0].secondLevel);
+        }
     };
 
     return (
         <div className="flex flex-row-reverse">
-            <div className="next-page-container">
-                <p>Next</p>
-                <div className="next-page-btn" onClick={handleClick}>
-                    {docName}
+            {route && (
+                <div className="next-page-container">
+                    <p>Next</p>
+                    <div className="next-page-btn" onClick={handleClick}>
+                        {docName}
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 };
