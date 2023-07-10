5911c1ec0267c5a8a6da7012e5ad09473de53432
lee920217 <francislee920217@gmail.com>
2023-07-10 15:18:03 +0800

    update copy btn details

--- src/components/CopyButton/CopyButton.tsx
+++ src/components/CopyButton/CopyButton.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import CopyBtn from '../../assets/img/copy.png'
+import CopySelectorBtn from '../../assets/img/copy-selector.png'
 import './CopyButton.scss'
 
 interface CopyButtonProps {
@@ -7,30 +8,50 @@ interface CopyButtonProps {
 }
 
 const CopyButton: React.FC<CopyButtonProps> = ({ text }) => {
+    const [popoverText, setPopoverText] = useState('');
     const [showPopover, setShowPopover] = useState(false);
+    const [hovering, setHovering] = useState(false);
 
     const copyText = async () => {
         await navigator.clipboard.writeText(text);
+        setPopoverText('Copied!');
         setShowPopover(true);
         setTimeout(() => {
-            setShowPopover(false);
+            if (!hovering) { // Only hide if not hovering
+                setShowPopover(false);
+            }
         }, 2000);
     };
+
+    const mouseEnter = () => {
+        setHovering(true);
+        if (!showPopover) {
+            setPopoverText('Copy Code');
+            setShowPopover(true);
+        }
+    }
+
+    const mouseLeave = () => {
+        setHovering(false);
+        setShowPopover(false);
+    }
 
     return (
         <div className="copy-btn-wrapper">
             {showPopover &&
                 <div className="popover">
-                    Copied!
+                    {popoverText}
                     <div className="popover-arrow"></div>
                 </div>
             }
             <img
                 className="code-block-copy-btn"
-                src={CopyBtn}
+                src={hovering ? CopySelectorBtn : CopyBtn}
                 width={18}
                 height={18}
                 onClick={copyText}
+                onMouseEnter={mouseEnter}
+                onMouseLeave={mouseLeave}
                 alt="Copy button"
             />
         </div>
